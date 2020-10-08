451d91abf339dab804149a46d8ea89bc86e568a4
Jessica Salbert <jessicarsalbert@gmail.com>
2020-10-08 00:13:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
+  return fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  return json
+  
 }
 
 function renderBooks(books) {
